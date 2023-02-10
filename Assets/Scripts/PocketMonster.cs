1ccaefe881be6982b133217b628680c4826013b2
Tom Scott <48756858+TomDotScott@users.noreply.github.com>
2023-02-10 15:03:45 +0000

    Outline PocketMonster.cs

---
+++ Assets/Scripts/PocketMonster.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PocketMonster : MonoBehaviour
+{
+    public enum Element
+    {
+        Normal,
+        Fire,
+        Water,
+        Grass
+    }
+
+    public Element Type { get; private set; }
+    [SerializeField] private Element m_type;
+
+    [System.Serializable]
+    public struct Stats
+    {
+        public float m_attack;
+        public float m_defense;
+        public float m_hp;
+        public float m_speed;
+    }
+
+    private Ability m_ability;
+
+    [SerializeField] private Stats m_stats;
+
+    [SerializeField] private Move[] m_moves = new Move[4];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

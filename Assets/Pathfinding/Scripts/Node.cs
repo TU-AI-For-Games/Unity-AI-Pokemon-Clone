83599250c6ba8f5fe57c16776a14c8bfea58128b
emwaAdventure <emwa2000@hotmail.com>
2023-02-17 18:07:04 +0100

    Continue A*

--- Assets/Pathfinding/Scripts/Node.cs
+++ Assets/Pathfinding/Scripts/Node.cs
@@ -6,23 +6,86 @@ using UnityEngine;
 
 public class Node
 {
-    public float radius;
-    public Vector3 position;
-    public bool walkable = true;
-    private LayerMask unwalkableMask;
+    
+    // G cost is the cost from start to this node
+    public float gCost;
+    // H cost is the cost from this to the end
+    public float hCost;
+    // F cost is the combined cost
+    public float fCost => gCost + hCost;
 
-    public Node(float radius, Vector3 position, LayerMask unwalkableMask)
+    private float m_radius;
+    private Vector3 m_worldPosition;
+    private Vector2Int m_gridPosition;
+    private bool m_walkable = true;
+    private LayerMask m_unWalkableMask;
+    private bool m_selected;
+    private Node m_parentNode = null;
+
+    public Node(float radius, Vector3 worldPosition, Vector2Int gridPosition, LayerMask unwalkableMask)
     {
-        this.radius = radius;
-        this.position = position;
-        this.unwalkableMask = unwalkableMask;
+        m_radius = radius;
+        m_worldPosition = worldPosition;
+        m_gridPosition = gridPosition;
+        m_unWalkableMask = unwalkableMask;
         CheckWalkable();
     }
 
     private void CheckWalkable()
     {
-        walkable = !Physics.CheckSphere(position, radius, unwalkableMask);
+        m_walkable = !Physics.CheckSphere(m_worldPosition, m_radius, m_unWalkableMask);
     }
+
+    public void SetParent(Node newParent)
+    {
+        m_parentNode = newParent;
+    }
+    
+    public Node GetParent()
+    {
+        return m_parentNode;
+    }
+
+    public bool IsWalkable()
+    {
+        return m_walkable;
+    }
+    
+    public void SetWalkable(bool isWalkable)
+    {
+        m_walkable = isWalkable;
+    }
+
+    public Vector3 GetNodeWorldPosition()
+    {
+        return m_worldPosition;
+    }
+    
+    public Vector2Int GetNodeGridPosition()
+    {
+        return m_gridPosition;
+    }
+    
+    public void SetNodeWorldPosition(Vector3 newWorldLocation)
+    {
+        m_worldPosition = newWorldLocation;
+    }
+
+    public float GetNodeRadius()
+    {
+        return m_radius;
+    }
+    
+    public void SetSelected(bool selected)
+    {
+        m_selected = selected;
+    }
+    
+    public bool IsSelected()
+    {
+        return m_selected;
+    }
+    
     
     
 }
--- Assets/Pathfinding/Scripts/Pathfinding.cs
+++ Assets/Pathfinding/Scripts/Pathfinding.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [ExecuteInEditMode]
 public class Pathfinding : MonoBehaviour
@@ -15,9 +17,16 @@ public class Pathfinding : MonoBehaviour
     public float maxWalkableAngle = 30;
     [SerializeField]
     public bool showDebug = false;
+    [SerializeField]
+    public int iterations = 10;
+
+    [SerializeField] public GameObject startingObject;
+    [SerializeField] public GameObject targetObject;
 
     [SerializeField] 
-    public LayerMask unwalkableMask;
+    public LayerMask unWalkableMask;
+    [SerializeField] 
+    public LayerMask walkableMask;
     [SerializeField]
     public float nodeRadius = 1;
     [SerializeField]
@@ -37,33 +46,32 @@ public class Pathfinding : MonoBehaviour
         {
             for (int z = 0; z < gridSize; z++)
             {
-
-                RaycastHit rayHit;
-                Vector3 offset = location + new Vector3(x * nodeRadius, 0, z * nodeRadius);
+                var offset = location + new Vector3(x * nodeRadius, 0, z * nodeRadius);
 
 
-                // Trace to find the ground
-                bool hit = Physics.Linecast(offset + new Vector3(0, 100, 0), offset + new Vector3(0, -100, 0), out rayHit);
+                // Trace to find the walkable ground
+                var hit = Physics.Linecast(offset + new Vector3(0, 100, 0), offset + new Vector3(0, -100, 0), out var rayHit, walkableMask);
                 
                 
-                nodes[x, z] = MakeNode(offset);
+                nodes[x, z] = MakeNode(offset, new Vector2Int(x,z));
 
                 if (hit) 
                 {
 
-                    // If the angle of the terrain is more than the max walkable angle, it will be unwalkable
+                    // If the angle of the terrain is more than the max walkable angle, it will be un-walkable
                     if(Vector3.Angle(Vector3.up, rayHit.normal) > maxWalkableAngle)
                     {
-                        nodes[x, z].walkable = false;
+                        nodes[x, z].SetWalkable(false);
                     }
 
                     // Set the Y position of the node to the terrain
-                    nodes[x, z].position.y = rayHit.point.y;
+                    var OldPos = nodes[x, z].GetNodeWorldPosition();
+                    nodes[x, z].SetNodeWorldPosition(new Vector3(OldPos.x, rayHit.point.y, OldPos.z));
                 }
                 else
                 {
-                    // This means the ray did not hit anything, making an unwalkable node
-                    nodes[x, z].walkable = false;
+                    // This means the ray did not hit anything, making an un-walkable node
+                    nodes[x, z].SetWalkable(false);
                 }
 
             }
@@ -73,42 +81,171 @@ public class Pathfinding : MonoBehaviour
 
     bool FindPath(Vector3 startPosition, Vector3 targetPosition)
     {
+        
+        // Get the nodes
+        var startNode = GetNodeFromPosition(startPosition);
+        var endNode = GetNodeFromPosition(targetPosition);
 
+        // If one of them is not valid, cancel
+        if (startNode == null || endNode == null) return false;
+        
+        // Select the nodes
+        startNode.SetSelected(true);
+        endNode.SetSelected(true);
+        
+        // Make the Open and Closed lists
+        List<Node> OpenNodes = new List<Node>();
+        List<Node> ClosedNodes = new List<Node>();
+        
+        OpenNodes.Add(startNode);
+        
+        
+        var currentNode = startNode;
+    
+        // While we have not yet reached the target node
 
+        for (int i = 0; i < iterations; i++)
+        {
+            currentNode = GetLowestCost(OpenNodes, endNode);
+            OpenNodes.Remove(currentNode);
+            ClosedNodes.Add(currentNode);
 
+            // If the path is found, return
+            if (currentNode == endNode)
+            {
+              print("Path Found!");
+              return true;
+            }
 
+            var neighbors = GetNeighboringNodes(currentNode);
+
+            foreach (var neighbor in neighbors)
+            {
+                // Check if the neighbor is valid, if not, continue to next
+                if (!neighbor.IsWalkable() || ClosedNodes.Contains(neighbor)) continue;
+                
+                
+                
+            }
+
+        }
+        
+       
 
         return false;
+
     }
 
 
     private void Update()
     {
         MakeGrid(transform.position);
+        FindPath(startingObject.transform.position, targetObject.transform.position);
+
     }
 
-    Node MakeNode(Vector3 position)
+    private Node MakeNode(Vector3 worldPosition, Vector2Int gridPosition)
     {
-        return new Node(nodeRadius, position, unwalkableMask);
+        return new Node(nodeRadius, worldPosition, gridPosition, unWalkableMask);
+    }
+
+    private Node GetLowestCost(List<Node> openNodes, Node endNode)
+    {
+        Node currentNext = null;
+
+        int index = 1;
+        foreach (var node in openNodes)
+        {
+            if (currentNext == null)
+            {
+                currentNext = node;
+                continue;
+            }
+            
+            var newGCost = index % 2 == 0 ? straightCost : diagonalCost;
+            var newHCost = Vector2Int.Distance(node.GetNodeGridPosition(), endNode.GetNodeGridPosition());
+
+            node.gCost = newGCost + node.GetParent().gCost;
+            node.hCost = Mathf.RoundToInt(newHCost);
+
+            if (node.fCost < currentNext.fCost)
+            {
+                currentNext = node;
+            }
+
+            index++;
+
+        }
+        return currentNext;
+        
+    }
+    
+    
+
+    private Node GetNodeFromPosition(Vector3 position)
+    {
+        // Subtract world position to get local node position
+        position -= transform.position;
+        
+        // Round it to work with the scale of the grid
+        Vector3Int gridPosition = Vector3Int.RoundToInt(position / nodeRadius);
+        
+        // Make Coordinates from the position
+        Vector2Int nodeCoords = new Vector2Int(gridPosition.x, gridPosition.z);
+
+        // Return the node if the coordinates is valid
+        return NodeExists(nodeCoords) ? nodes[nodeCoords.x, nodeCoords.y] : null;
+    }
+
+    private bool NodeExists(Vector2Int nodeCoords)
+    {
+        // Node Coordinates are outside the grid
+        if (nodeCoords.x > nodes.GetLength(0) || nodeCoords.y > nodes.GetLength(1)) return false;
+
+        // Node Coordinates are negative
+        if (nodeCoords.x < 0 || nodeCoords.y < 0) return false;
+
+        
+        // Node Coords are valid
+        return true;
     }
 
 
-    Node GetNodeFromPosition(Vector3 position)
+    // Lookup table for neighbor Nodes
+    private readonly Vector2Int[] _nodeNeighbors =
     {
 
-        float gridSize = 1f;
-        Vector3 gridPosition = Vector3Int.FloorToInt(position / gridSize);
-        var coords = gridPosition * gridSize;
+        new (-1, -1),
+        new (0, -1),
+        new (1, -1),
+        new (1, 0),
+        new (1, 1),
+        new (0, 1),
+        new (-1, 1),
+        new (-1, 0)
 
+    };
 
+    private List<Node> GetNeighboringNodes(Node parentNode)
+    {
 
+        var neighbors = new List<Node>();
 
+        
+        // Loop through all the eight neighbors and add them to the neighbors list
+        
+        for (var i = 0; i < 8; i++)
+        {
+            var offset = parentNode.GetNodeGridPosition() + _nodeNeighbors[i];
+            if (!NodeExists(offset)) continue;
+            
+            var newNode = nodes[offset.x, offset.y];
+            neighbors.Add(newNode);
+        }
 
-        return null;
-
-
+        return neighbors;
     }
-
+    
     private void OnDrawGizmos()
     {
 
@@ -119,11 +256,12 @@ public class Pathfinding : MonoBehaviour
 
         foreach (var node in nodes)
         {
+        
+            var radius = node.GetNodeRadius() * 0.9f;
+            Gizmos.color = node.IsSelected() ? Color.blue : node.IsWalkable() ? Color.green : Color.red;
+            Gizmos.DrawSphere(node.GetNodeWorldPosition(), radius/4);
+            Handles.Label(node.GetNodeWorldPosition() + new Vector3(0,1,0), node.fCost.ToString());
 
-            var radius = node.radius * 0.9f;
-            Gizmos.color = node.walkable ? Color.green : Color.red;
-            Gizmos.DrawSphere(node.position, radius/4);
-            
         }
     }
 }

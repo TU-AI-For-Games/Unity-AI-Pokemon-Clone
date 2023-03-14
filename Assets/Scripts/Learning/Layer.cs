81bef3778c66ef120c27f183ddb494c57ebcccdf
Tom Scott <tom.scott45456@gmail.com>
2023-03-14 22:27:37 +0000

    Initialise Layers
    The neural network has a list of hidden layers and an output layer

---
+++ Assets/Scripts/Learning/Layer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Learning
+{
+    public class Layer
+    {
+        public float[,] Weights;
+        public float[] Biases;
+        public float[] Nodes;
+
+        private int m_numNodes;
+        private int m_numInputs;
+
+        public Layer(int numInputs, int numNodes)
+        {
+            m_numInputs = numInputs;
+            m_numNodes = numNodes;
+
+            Weights = new float[numNodes, numInputs];
+            Biases = new float[numNodes];
+            Nodes = new float[numNodes];
+        }
+    }
+}
--- Assets/Scripts/Learning/NeuralNetwork.cs
+++ Assets/Scripts/Learning/NeuralNetwork.cs
@@ -1,18 +1,31 @@
-using System.Collections;
 using System.Collections.Generic;
+using Learning;
 using UnityEngine;
 
 public class NeuralNetwork : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField][Range(1, 1000)] private int m_numHiddenLayers = 2;
+    private List<Layer> m_hiddenLayers;
+    private Layer m_outputLayer;
+
+    void Awake()
     {
-        
+        m_hiddenLayers = new List<Layer>(m_numHiddenLayers)
+        {
+            new(2, 4)
+        };
+
+        for (int i = 0; i < m_numHiddenLayers - 1; ++i)
+        {
+            m_hiddenLayers.Add(new Layer(4, 4));
+        }
+
+        m_outputLayer = new Layer(4, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }

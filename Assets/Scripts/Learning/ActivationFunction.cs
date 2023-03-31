095d23f86c9b84ea800d9a3a03dfdacc151f3df3
Tom Scott <tom.scott45456@gmail.com>
2023-03-31 12:25:39 +0100

    Activation function interface

---
+++ Assets/Scripts/Learning/ActivationFunction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Learning
+{
+    public interface IActivationFunction
+    {
+        public double Compute(double value);
+        public double Derive(double value);
+    }
+
+}

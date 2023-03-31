7612426f878eacde2d8318997fa8028879acf5ed
Tom Scott <tom.scott45456@gmail.com>
2023-03-31 12:26:32 +0100

    Synapse class

---
+++ Assets/Scripts/Learning/Synapse.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Learning
+{
+    public class Synapse
+    {
+        public Neuron Input { get; set; }
+        public Neuron Output { get; set; }
+        public double Weight { get; set; }
+        public double DeltaWeight { get; set; }
+
+        public Synapse(Neuron input, Neuron output)
+        {
+            Input = input;
+            Output = output;
+            Weight = NeuralNetwork.GetRandomInitialisationValue(InitialisationMode.Xavier);
+        }
+    }
+}

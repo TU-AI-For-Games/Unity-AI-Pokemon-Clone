7220cac8f7f2ab1631d8e0cfd1d90fc4a2761ea0
Emil Walseth <45356064+emilwalseth@users.noreply.github.com>
2023-04-24 14:29:44 +0100

    Debug

--- Assets/Scripts/Pathfinding/TestPawnPathfinding.cs
+++ Assets/Scripts/Pathfinding/TestPawnPathfinding.cs
@@ -10,10 +10,55 @@ public class TestPawnPathfinding : MonoBehaviour
 
     [SerializeField] private GameObject m_target;
     [SerializeField] private Pathfinding m_pathfinding;
+    [SerializeField] private float m_acceptanceRadius = 3;
+    [SerializeField] private float m_speed = 5;
+
+
+    private List<Node> path;
     
+    private void MakePath()
+    {
+        path = m_pathfinding.FindPath(transform.position, m_target.transform.position);
+    }
+
+    private void Start()
+    {
+        MakePath();
+    }
+
     private void OnDrawGizmos()
     {
-        List<Node> path = m_pathfinding.FindPath(transform.position, m_target.transform.position);
-
+        if (Application.isPlaying) return;
+        
+        MakePath();
     }
+
+    private void Update()
+    {
+        if (!Application.isPlaying) return;
+
+        if (path == null) return;
+
+        if (path.Count == 0) return;
+ 
+        MoveTowards();
+  
+    }
+
+    private void MoveTowards()
+    {
+        if (path == null || path.Count < 1) return;
+
+        Vector3 nextPoint = path[0].GetNodeWorldPosition();
+
+        // If distance is less than the acceptanceradius, remove it and go to next point
+        if (Vector3.Distance(transform.position, nextPoint) < m_acceptanceRadius)
+        {
+            path.RemoveAt(0);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPoint, m_speed * Time.deltaTime);
+    }
+
+
 }

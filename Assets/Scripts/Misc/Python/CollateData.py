c7d0b9546e1f5bd3507d54f7de3ecf3d3394ca0c
Tom Scott <tom.scott45456@gmail.com>
2023-04-25 21:28:21 +0100

    Normalise the collated data

--- Assets/Scripts/Misc/Python/CollateData.py
+++ Assets/Scripts/Misc/Python/CollateData.py
@@ -1,4 +1,35 @@
-import os
+import os, math
+
+def calculate_hp_stat(base, iv, ev, level):
+    return int(((base + iv) * 2 + math.sqrt(ev) / 4) * level / 100) + level + 10;
+
+
+def load_pokemon_dictionary():
+    # ID : HEALTH
+    pokemon = dict()
+
+    # Load the data about the pokemon's HP
+    with open("..\\..\\..\\Resources\\Data\\pokemon.csv", 'r') as pokemon_data:
+        lines = pokemon_data.readlines()
+
+    for i in range(1, len(lines)):
+        mon = lines[i].split(',')
+        pokemon[int(mon[0])] = calculate_hp_stat(int(mon[3]), 15, 256, 50)
+    return pokemon
+
+
+def normalise_data(battleData):
+    data = battleData.split(',')
+    # Player Pkmn ID,Target Pkmn ID,Player HP,Target HP,Player Element,Target Element,Attack?,Switch?,Heal?
+    playerId = int(data[0]) / 151
+    targetId = int(data[2]) / 151
+    playerHealth = int(data[2]) / pokeData[int(data[0])]
+    targetHealth = int(data[3]) / pokeData[int(data[1])]
+    playerElement = int(data[4]) / 17
+    targetElement = int(data[5]) / 17
+    dataString = f"{playerId},{targetId},{playerHealth},{targetHealth},{playerElement},{targetElement},{','.join(data[-3:])}"
+    return dataString
+
 
 def generate_output_line(battleData):
     # Chosen Action (Attack/Switch/Heal),Player Pkmn ID,Target Pkmn ID,Player HP,Target HP,Player Element,Target Element,Previous Pkmn ID,Previous Pkmn Element,Chosen Move ID,Move Element,STAB?,Move Power,User Status,Status Move?,Status Hit?,Status Applied,Hit?,Effectiveness(0-2),Stat Change?,Stat Changed,Stat Target(T/U),Kill?,Outcome Target HP
@@ -8,8 +39,8 @@ def generate_output_line(battleData):
     outcome = ["0", "0", "0"]
     outcome[int(data[0])] = "1"
 
-    dataString = f"{','.join(data[1:7])},{','.join(outcome)}\n"
-    return dataString
+    dataString = normalise_data(f"{','.join(data[1:7])},{','.join(outcome)}")
+    return dataString + '\n'
 
 
 def parse_battle_data_file(filename, outFile):
@@ -19,6 +50,8 @@ def parse_battle_data_file(filename, outFile):
     for i in range(1, len(lines)):
         outFile.write(generate_output_line(lines[i]))
 
+
+pokeData = load_pokemon_dictionary()
 
 data_folder = "C:\\Users\\tomsc\\AppData\\Roaming\\AiPokeClone"
 

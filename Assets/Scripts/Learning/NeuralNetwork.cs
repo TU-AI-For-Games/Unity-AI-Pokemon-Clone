c94d7db0e57d354814971c898ce8254a4b94b9f8
Tom Scott <tom.scott45456@gmail.com>
2023-04-25 21:42:43 +0100

    Move Neural Network class to the Learning namespace

--- Assets/Scripts/Learning/NeuralNetwork.cs
+++ Assets/Scripts/Learning/NeuralNetwork.cs
@@ -1,142 +1,144 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using Learning;
 using UnityEngine;
 
-public class NeuralNetwork
+namespace Learning
 {
-    private int[] m_networkShape;
-    private List<Layer> m_layers;
-
-    // The folder that the ANN will be saved out to when serialised
-    private readonly string m_outputFolderName = $"{Application.dataPath}\\Resources\\NeuralNets";
-
-    public NeuralNetwork(int[] networkShape, float learningRate, Layer.ActivationFunction activationFunction)
+    public class NeuralNetwork
     {
-        m_networkShape = new int[networkShape.Length];
+        private int[] m_networkShape;
+        private List<Layer> m_layers;
 
-        for (int i = 0; i < networkShape.Length; i++)
+        // The folder that the ANN will be saved out to when serialised
+        private readonly string m_outputFolderName = $"{Application.dataPath}\\Resources\\NeuralNets";
+
+        public NeuralNetwork(int[] networkShape, float learningRate, Layer.ActivationFunction activationFunction)
         {
-            m_networkShape[i] = networkShape[i];
-        }
+            m_networkShape = new int[networkShape.Length];
 
-        m_layers = new List<Layer>();
-
-        for (int i = 0; i < networkShape.Length - 1; ++i)
-        {
-            m_layers.Add(new Layer(networkShape[i], networkShape[i + 1], learningRate, activationFunction));
-        }
-    }
-
-    public void Train(List<LearningData> trainingData, int numEpochs)
-    {
-        for (int i = 0; i < numEpochs; i++)
-        {
-            foreach (LearningData data in trainingData)
+            for (int i = 0; i < networkShape.Length; i++)
             {
-                FeedForward(data.Targets);
-                BackPropagation(data.Values);
+                m_networkShape[i] = networkShape[i];
             }
-        }
-    }
 
-    public float[] Compute(float[] input)
-    {
-        FeedForward(input);
-        return m_layers[^1].Outputs;
-    }
+            m_layers = new List<Layer>();
 
-    private void FeedForward(float[] input)
-    {
-        m_layers[0].FeedForward(input);
-
-        for (int i = 1; i < m_layers.Count; i++)
-        {
-            m_layers[i].FeedForward(m_layers[i - 1].Outputs);
-        }
-    }
-
-    private void BackPropagation(float[] expected)
-    {
-        for (int i = m_layers.Count - 1; i >= 0; i--)
-        {
-            if (i == m_layers.Count - 1)
+            for (int i = 0; i < networkShape.Length - 1; ++i)
             {
-                m_layers[i].BackPropagationOutputLayer(expected);
-            }
-            else
-            {
-                m_layers[i].BackPropagationHiddenLayer(m_layers[i + 1].Gamma, m_layers[i + 1].Weights);
+                m_layers.Add(new Layer(networkShape[i], networkShape[i + 1], learningRate, activationFunction));
             }
         }
 
-        // Update the weights of the layers...
-        foreach (Layer layer in m_layers)
+        public void Train(List<LearningData> trainingData, int numEpochs)
         {
-            layer.UpdateWeights();
-        }
-    }
-
-    public void Save(string filename)
-    {
-        string filePath = $"{m_outputFolderName}\\{filename}";
-
-        File.WriteAllText(filePath, string.Empty);
-
-        FileStream outStream = File.OpenWrite(filePath);
-
-        StreamWriter writer = new StreamWriter(outStream);
-
-        // Network shape
-        string networkShape = string.Join(',', m_networkShape);
-        writer.WriteLine(networkShape);
-
-        // Each Layer on new line
-        foreach (Layer layer in m_layers)
-        {
-            layer.Save(writer);
-        }
-
-        writer.Close();
-        outStream.Close();
-    }
-
-    public void Load(string filename)
-    {
-        string filePath = $"{m_outputFolderName}\\{filename}";
-
-        string text = File.ReadAllText(filePath);
-        string[] lines = text.Split(Environment.NewLine);
-
-        m_layers = new List<Layer>();
-
-        for (int i = 0; i < lines.Length; ++i)
-        {
-            if (lines[i].Length == 0)
+            for (int i = 0; i < numEpochs; i++)
             {
-                continue;
-            }
-
-            // Network details
-            if (i == 0)
-            {
-                string[] networkDetails = lines[i].Split(',');
-
-                m_networkShape = new int[networkDetails.Length];
-
-                for (int j = 0; j < networkDetails.Length; ++j)
+                foreach (LearningData data in trainingData)
                 {
-                    m_networkShape[j] = int.Parse(networkDetails[j]);
+                    FeedForward(data.Targets);
+                    BackPropagation(data.Values);
                 }
             }
-            else
-            {
-                // Layer details
-                Layer layer = new Layer();
-                layer.Load(lines[i]);
+        }
 
-                m_layers.Add(layer);
+        public float[] Compute(float[] input)
+        {
+            FeedForward(input);
+            return m_layers[^1].Outputs;
+        }
+
+        private void FeedForward(float[] input)
+        {
+            m_layers[0].FeedForward(input);
+
+            for (int i = 1; i < m_layers.Count; i++)
+            {
+                m_layers[i].FeedForward(m_layers[i - 1].Outputs);
+            }
+        }
+
+        private void BackPropagation(float[] expected)
+        {
+            for (int i = m_layers.Count - 1; i >= 0; i--)
+            {
+                if (i == m_layers.Count - 1)
+                {
+                    m_layers[i].BackPropagationOutputLayer(expected);
+                }
+                else
+                {
+                    m_layers[i].BackPropagationHiddenLayer(m_layers[i + 1].Gamma, m_layers[i + 1].Weights);
+                }
+            }
+
+            // Update the weights of the layers...
+            foreach (Layer layer in m_layers)
+            {
+                layer.UpdateWeights();
+            }
+        }
+
+        public void Save(string filename)
+        {
+            string filePath = $"{m_outputFolderName}\\{filename}";
+
+            File.WriteAllText(filePath, string.Empty);
+
+            FileStream outStream = File.OpenWrite(filePath);
+
+            StreamWriter writer = new StreamWriter(outStream);
+
+            // Network shape
+            string networkShape = string.Join(',', m_networkShape);
+            writer.WriteLine(networkShape);
+
+            // Each Layer on new line
+            foreach (Layer layer in m_layers)
+            {
+                layer.Save(writer);
+            }
+
+            writer.Close();
+            outStream.Close();
+        }
+
+        public void Load(string filename)
+        {
+            string filePath = $"{m_outputFolderName}\\{filename}";
+
+            string text = File.ReadAllText(filePath);
+            string[] lines = text.Split(Environment.NewLine);
+
+            m_layers = new List<Layer>();
+
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                if (lines[i].Length == 0)
+                {
+                    continue;
+                }
+
+                // Network details
+                if (i == 0)
+                {
+                    string[] networkDetails = lines[i].Split(',');
+
+                    m_networkShape = new int[networkDetails.Length];
+
+                    for (int j = 0; j < networkDetails.Length; ++j)
+                    {
+                        m_networkShape[j] = int.Parse(networkDetails[j]);
+                    }
+                }
+                else
+                {
+                    // Layer details
+                    Layer layer = new Layer();
+                    layer.Load(lines[i]);
+
+                    m_layers.Add(layer);
+                }
             }
         }
     }
